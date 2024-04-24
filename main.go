e6fca49d4860066f8bfde35c205f620b8879108b
Jlogblpb <Jlogblpb@yandex.ru>
2024-04-24 23:47:49 +0300

    edit wbsrv

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
 func main() {
+	// создаем новый роутер
+	r := chi.NewRouter()
+
+	// регистрируем в роутере эндпоинт `/` с методом getIndex
+	r.Get("/", getIndex)
+
+	// регистрируем в роутере эндпоинт `/artists` с методом POST,
+	// для которого используется обработчик `postArtist`
+	r.Post("/artists", postArtist)
+	// регестрируем в роутере эндпоинт `/artist/{id}`  с методом GETб,
+	// для которого используется обработчик `getArtist`
+	r.Get("/artist/{id}", getArtist)
+
+	// Регистрируем роутер Русский рок
+	r.Get("/russian_rock/{id}", getRusArtists)
+
+	// запускаем сервер
+	if err := http.ListenAndServe(":7540", r); err != nil {
+		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
+		return
+	}
 
 }
