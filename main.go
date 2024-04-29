c0c1aa6049afe6a571e609e60211e12c2a445de7
Jlogblpb <Jlogblpb@yandex.ru>
2024-04-30 01:19:32 +0300

    1.9-complite

--- main.go
+++ main.go
@@ -1,33 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	// создаем новый роутер
-	r := chi.NewRouter()
 
-	// регистрируем в роутере эндпоинт `/` с методом getIndex
-	r.Get("/", getIndex)
+	webDir := "./web"
 
-	// регистрируем в роутере эндпоинт `/artists` с методом POST,
-	// для которого используется обработчик `postArtist`
-	r.Post("/artists", postArtist)
-	// регестрируем в роутере эндпоинт `/artist/{id}`  с методом GETб,
-	// для которого используется обработчик `getArtist`
-	r.Get("/artist/{id}", getArtist)
+	http.Handle("/", http.FileServer(http.Dir(webDir)))
+	http.Handle("/js/scripts.min.js", http.FileServer(http.Dir(webDir)))
+	http.Handle("/css/style.css", http.FileServer(http.Dir(webDir)))
+	http.Handle("/favicon.ico", http.FileServer(http.Dir(webDir)))
 
-	// Регистрируем роутер Русский рок
-	r.Get("/russian_rock/{id}", getRusArtists)
-
-	// запускаем сервер
-	if err := http.ListenAndServe(":7540", r); err != nil {
-		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
-		return
+	err := http.ListenAndServe(":7540", nil)
+	if err != nil {
+		panic(err)
 	}
-
 }
