deaa8e11b2cf4b3cbf0ed01d5e00884df72de611
Jlogblpb <Jlogblpb@yandex.ru>
2024-05-05 22:59:23 +0300

    create BD, but test failed

--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module final-main
 go 1.22.2
 
 require (
-	github.com/go-chi/chi/v5 v5.0.12
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/mattn/go-sqlite3 v1.14.22
 	github.com/stretchr/testify v1.9.0
--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+// Создаем БД
+func createDB() {
+	//создаем непосредственно БД
+	fileDB, err := sql.Open("sqlite3", "./scheduler.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//создаем таблицу
+	new_table, err := fileDB.Prepare("CREATE TABLE scheduler (id INTGEER PRIMARY KEY, date CHAR(8), title TEXT, comment TEXT, repeat VARCHAR(128))")
+	if err != nil {
+		log.Fatal(err)
+	}
+	new_table.Exec()
+	//Сделать date индексируемой
+	crateIndex, err := fileDB.Prepare("CREATE INDEX indexdate ON scheduler (date)")
+	crateIndex.Exec()
+	fileDB.Close()
+}
 
 func main() {
 
+	// Работа с БД
+
+	//Проверяем БД на наличие
+	appPath, err := os.Getwd()
+	//Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbFile := filepath.Join(appPath, "scheduler.db")
+	_, err = os.Stat(dbFile)
+
+	var install bool
+	if err != nil {
+		install = true
+	}
+
+	// если install равен true, после открытия БД требуется выполнить
+	// sql-запрос с CREATE TABLE и CREATE INDEX
+
+	//Создаем БД
+	if install == true {
+		createDB()
+	} else {
+		db, err := sql.Open("sqlite3", "./scheduler.db")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+	}
+
+	//Запуск WEB сервера
 	webDir := "./web"
 
 	http.Handle("/", http.FileServer(http.Dir(webDir)))
@@ -13,8 +69,9 @@ func main() {
 	http.Handle("/css/style.css", http.FileServer(http.Dir(webDir)))
 	http.Handle("/favicon.ico", http.FileServer(http.Dir(webDir)))
 
-	err := http.ListenAndServe(":7540", nil)
+	err = http.ListenAndServe(":7540", nil)
 	if err != nil {
 		panic(err)
 	}
+
 }
